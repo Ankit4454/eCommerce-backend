10eb01781968c7d8eb517b3a0c6971898e21563d
Ankit4454 <kanteliyaankit@gmail.com>
2024-01-28 20:14:41 +0530

    info: Create address function changes

--- controllers/address_controller.js
+++ controllers/address_controller.js
@@ -1,13 +1,25 @@
 const Address = require('../models/address');
+const User = require('../models/user');
 
 module.exports.create = function (req, res) {
     Address.create({ user: req.body.user, name: req.body.name, mobileNumber: req.body.mobileNumber, pincode: req.body.pincode, addressLine1: req.body.addressLine1, addressLine2: req.body.addressLine2, landmark: req.body.landmark, city: req.body.city, state: req.body.state, addressType: req.body.addressType }).then(function (address) {
-        return res.status(201).json({
-            success: true,
-            data: {
-                address: address,
-            },
-            message: 'Address created successfully'
+        User.findById(address.user).then(function (user) {
+            user.addressList.push(address._id);
+            return user.save().then(function () {
+                return res.status(201).json({
+                    success: true,
+                    data: {
+                        address: address,
+                    },
+                    message: 'Address created successfully'
+                });
+            });
+        }).catch(function (err) {
+            console.log(`Error while fetching a user ${err}`);
+            return res.status(500).json({
+                error: true,
+                message: err.message || 'Internal Server Error'
+            });
         });
     }).catch(function (err) {
         console.log(`Error while creating a address ${err}`);
